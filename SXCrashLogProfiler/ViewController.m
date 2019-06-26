242b113a5e7fda2e5097d7839e7dddee6588914d
sunxin <sunxin@smzdm.com>
2019-06-26 22:34:00 +0800

    描述更改
    按钮描述更改

--- SXCrashLogProfiler/ViewController.m
+++ SXCrashLogProfiler/ViewController.m
@@ -21,14 +21,14 @@
     
     UIButton * btn = [UIButton buttonWithType:UIButtonTypeCustom];
     btn.frame = CGRectMake(100, 200, 200, 30);
-    [btn setTitle:@"点击进入日历列表" forState:UIControlStateNormal];
+    [btn setTitle:@"点击进入crash列表" forState:UIControlStateNormal];
     [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [btn addTarget:self action:@selector(actionPushToList:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btn];
     
     UIButton * btnCrash = [UIButton buttonWithType:UIButtonTypeCustom];
     btnCrash.frame = CGRectMake(100, 300, 200, 30);
-    [btnCrash setTitle:@"点击crash" forState:UIControlStateNormal];
+    [btnCrash setTitle:@"点击生成crash" forState:UIControlStateNormal];
     [btnCrash setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
     [btnCrash addTarget:self action:@selector(actionCrash:) forControlEvents:UIControlEventTouchUpInside];
     [self.view addSubview:btnCrash];
